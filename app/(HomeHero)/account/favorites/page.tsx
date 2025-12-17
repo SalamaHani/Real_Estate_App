701691177f20d0e93cfa497eb0c045572b8c3fae
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-12-17 11:27:35 +0200

    posh code

--- app/(HomeHero)/account/favorites/page.tsx
+++ app/(HomeHero)/account/favorites/page.tsx
@@ -1,29 +1,23 @@
-// import ListingOfagent from "@/components/agents/ListingOfagent";
-// import Continer from "@/components/global/Continer";
-// import TitelSection from "@/components/global/TitelSection";
-// import { faveretlisting } from "@/utils/actions";
-// import { listing } from "@prisma/client";
-// import React from "react";
-// interface FavoriteItem {
-//   listing: listing;
-// }
-// async function page() {
-//   const fivaretlistinng: FavoriteItem[] = await faveretlisting();
+import ListingOfagent from "@/components/agents/ListingOfagent";
+import Continer from "@/components/global/Continer";
+import TitelSection from "@/components/global/TitelSection";
+import { faveretlisting } from "@/utils/actions";
+import React from "react";
+async function page() {
+  const fivaretlistinng = await faveretlisting();
 
-//   if (fivaretlistinng.length == 0)
-//     return (
-//       <Continer className="mt-20">
-//         <TitelSection text="you have no reviews yet" />
-//       </Continer>
-//     );
-//   return (
-//     <Continer className="mt-20">
-//       <TitelSection text="Your Favorites Listing?" />
-//       <ListingOfagent
-//         listing={fivaretlistinng.map((item: FavoriteItem) => item.listing)}
-//       />
-//     </Continer>
-//   );
-// }
+  if (fivaretlistinng.length == 0)
+    return (
+      <Continer className="mt-20">
+        <TitelSection text="you have no reviews yet" />
+      </Continer>
+    );
+  return (
+    <Continer className="mt-20">
+      <TitelSection text="Your Favorites Listing?" />
+      <ListingOfagent listing={fivaretlistinng.map((item) => item.listing)} />
+    </Continer>
+  );
+}
 
-// export default page;
+export default page;
