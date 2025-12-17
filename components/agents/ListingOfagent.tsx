55b79e619a476e62779dffac6ba609babe52d1b5
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-12-17 13:41:38 +0200

    posh

--- components/agents/ListingOfagent.tsx
+++ components/agents/ListingOfagent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { listing } from "@prisma/client";
 import { CheckCircle, Clock, House, HousePlus, MapPin } from "lucide-react";
 import { Badge } from "../ui/badge";
 import {
